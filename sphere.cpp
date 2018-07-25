42c3851e6df737033589e7a9a4b77f5a2149b1ba
Hossam Samir <eng.hsamir@gmail.com>
2018-07-25 14:54:14 +0200

    implementation of sphere object

---
+++ sphere.cpp
@@ -0,0 +1,29 @@
+#include "sphere.h"
+sphere::sphere(/* args */)
+{
+    center = v3(0, 0, 0);
+    radius = 0;
+}
+
+sphere::sphere(const v3 &c, double r)
+{
+    radius = r;
+    center = c;
+}
+
+sphere::~sphere()
+{
+}
+
+sphere::sphere(const sphere & rhs)
+{
+    radius=rhs.radius;
+    center=rhs.center;
+}
+
+sphere& sphere::operator=(const sphere &rhs)
+{
+    radius=rhs.radius;
+    center=rhs.center;
+    return *this;
+}
---
+++ sphere.h
@@ -0,0 +1,18 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+#include "v3.h"
+class sphere
+{
+  private:
+    /* data */
+    double radius;
+    v3 center;
+
+  public:
+    sphere();
+    sphere(const v3 &c, double r); // arg constructor
+    sphere(const sphere &);
+    sphere &operator=(const sphere &rhs);
+    ~sphere();
+};
+#endif
